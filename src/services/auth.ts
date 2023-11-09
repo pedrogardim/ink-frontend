c15bb52dc8127f927e129c4702b33492cfcc4714
Pedro Gardim <me@pedrogardim.com>
2023-11-09 17:07:17 +0100

    feat: add redux auth api service

---
+++ src/services/auth.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL }),
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (loginBody) => ({
+        url: "auth/login",
+        method: "POST",
+        body: loginBody,
+      }),
+    }),
+    register: builder.mutation({
+      query: (registerBody) => ({
+        url: "auth/register",
+        method: "POST",
+        body: registerBody,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = authApi;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
+import { authApi } from "@/services/auth";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
