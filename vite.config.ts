54040661d2b46b029ad34eded0e4767b5a26bad3
Pedro Gardim <me@pedrogardim.com>
2023-11-09 18:13:15 +0100

    fix: add vite dev server proxy (CORS fix)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+      cors: "false",
+    },
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
