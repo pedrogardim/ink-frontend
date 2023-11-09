1859f1be268d69f246baac0049c27c8f3393cde9
Pedro Gardim <me@pedrogardim.com>
2023-11-09 13:03:16 +0100

    fix: page styling fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "@/common/layout/Header/Header";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="max-w-screen-xl h-screen mx-auto flex flex-col">
       <Header />
       <Routes>
         <Route path="/" element={<div>Home</div>} />
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </div>
   );
