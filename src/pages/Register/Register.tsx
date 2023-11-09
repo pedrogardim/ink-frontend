6335cd1be2dc1660a11bd0a93ffd28e7275c05f0
Pedro Gardim <me@pedrogardim.com>
2023-11-09 16:56:01 +0100

    feat: add validation on register form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Input, ImageInput } from "@/common";
+import useFormValidation from "@/hooks/useFormValidation";
+import clsx from "clsx";
 
 const helperText = `
 Welcome to our community!
@@ -13,16 +15,69 @@ const Register = () => {
   const [page, setPage] = useState(0);
   const navigate = useNavigate();
 
+  const { values, errors, onChange, onBlur, validateAll } = useFormValidation(
+    !page ? "registerFirstStep" : "registerSecondStep"
+  );
+
+  const hasErros = Object.keys(errors).length > 0;
+
+  const onNext = () => {
+    const isValid = validateAll();
+    if (!isValid) return;
+    if (!page) {
+      setPage(1);
+      return;
+    }
+    ///todo: api logic dispatch here
+  };
+
   const inputsPages = [
     <>
       <ImageInput />
-      <Input placeholder={"Your first name"} label={"First Name"} />
-      <Input placeholder={"Your last name"} label={"Last Name"} />
+      <Input
+        placeholder="Your first name"
+        label="First Name"
+        value={values.firstName}
+        onChange={(v) => onChange("firstName", v)}
+        onBlur={() => onBlur("firstName")}
+        error={errors.firstName}
+      />
+      <Input
+        placeholder="Your last name"
+        label="Last Name"
+        value={values.lastName}
+        onChange={(v) => onChange("lastName", v)}
+        onBlur={() => onBlur("lastName")}
+        error={errors.lastName}
+      />
     </>,
     <>
-      <Input placeholder={"Your email"} label={"Email"} />
-      <Input placeholder={"Your password"} label={"Password"} />
-      <Input placeholder={"Your phone number"} label={"Phone number"} />
+      <Input
+        placeholder="Your email"
+        label="Email"
+        value={values.email}
+        onChange={(v) => onChange("email", v)}
+        onBlur={() => onBlur("email")}
+        error={errors.email}
+      />
+      <Input
+        placeholder="Your password"
+        label="Password"
+        value={values.password}
+        onChange={(v) => onChange("password", v)}
+        onBlur={() => onBlur("password")}
+        error={errors.password}
+        type="password"
+      />
+      <Input
+        placeholder="Your phone number"
+        label="Phone number"
+        value={values.phoneNumber}
+        onChange={(v) => onChange("phoneNumber", v)}
+        onBlur={() => onBlur("phoneNumber")}
+        error={errors.phoneNumber}
+        type="number"
+      />
     </>,
   ];
   return (
@@ -42,7 +97,10 @@ const Register = () => {
         >
           {!page ? "Back to login" : "Back"}
         </button>
-        <button className="btn btn-primary" onClick={() => setPage(1)}>
+        <button
+          className={clsx("btn btn-primary", hasErros && "btn-disabled")}
+          onClick={onNext}
+        >
           {!page ? "Next" : "Create Account"}
         </button>
       </div>
