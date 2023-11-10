4d87f8071bf87e12eba606c1c020ef8202eff581
Pedro Gardim <me@pedrogardim.com>
2023-11-10 13:08:44 +0100

    feat: dynamic header based on auth user

--- src/common/layout/Header/Header.tsx
+++ src/common/layout/Header/Header.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
 import Icon from "@mdi/react";
 import { mdiBell, mdiMenu } from "@mdi/js";
+import { useSelector } from "@/store/hooks";
 
 const Header = () => {
+  const { user } = useSelector((state) => state.user);
   return (
     <div className="navbar bg-base-100">
       <div className="flex-none">
@@ -16,9 +19,11 @@ const Header = () => {
         <li>
           <a>Home</a>
         </li>
-        <li>
-          <a>Mis citas</a>
-        </li>
+        {user && (
+          <li>
+            <a>Mis citas</a>
+          </li>
+        )}
         <li>
           <a>Tatuadores</a>
         </li>
@@ -36,52 +41,64 @@ const Header = () => {
           </details>
         </li> */}
       </ul>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <Icon path={mdiBell} size={1} />
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </label>
-          <div
-            tabIndex={0}
-            className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="font-bold text-lg">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
+      {user && (
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <Icon path={mdiBell} size={1} />
+                <span className="badge badge-xs badge-primary indicator-item"></span>
+              </div>
+            </label>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+          <div className="dropdown dropdown-end">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar ml-2"
+            >
+              <div className="w-10 rounded-full">
+                <img src={user.profilePicUrl} />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar ml-2">
-            <div className="w-10 rounded-full">
-              <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg" />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
+      {!user && (
+        <Link to="/login" className="btn btn-outline btn-primary">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
