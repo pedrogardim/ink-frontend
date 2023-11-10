7fbaa961bd2b29d0a3effa282089929622229104
Pedro Gardim <me@pedrogardim.com>
2023-11-10 13:40:53 +0100

    feat: implement persistor on user profile

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./store";
+import { persistor, store } from "./store";
 
 import App from "./App.tsx";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,26 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
 import userReducer from "./slices/userSlice";
 import { authApi } from "@/services/auth";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    user: userReducer,
+    //TODO: fix ts error
+    user: persistReducer(persistConfig, userReducer),
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
