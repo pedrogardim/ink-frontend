339ee5fc257db37785a969b7958f3cc28dfb9dc4
Pedro Gardim <me@pedrogardim.com>
2023-11-12 18:41:14 +0100

    feat: add dummy appointments page

---
+++ src/pages/Appointments/Appointments.tsx
@@ -0,0 +1,13 @@
+const Appointments = () => {
+  return (
+    <div className="page">
+      <h1 className="text-2xl font-bold mr-auto">My appointments</h1>
+      <div className="flex flex-1 w-full flex-row items-stretch">
+        <div className="basis-1/4 border-r border-r-gray-600"></div>
+        <div className="basis-3/4">{}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Appointments;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export * from "./Home";
 export * from "./Login";
 export * from "./Register";
 export * from "./Profile";
+export * from "./Appointments";
