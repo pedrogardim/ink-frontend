7048cd64896d32217f639e7c6c2fdadf6c31c8db
Pedro Gardim <me@pedrogardim.com>
2023-11-17 10:51:39 +0100

    feat: add `TattooistGallery` component

---
+++ src/common/gallery/TattooistGallery/TattooistGallery.tsx
@@ -0,0 +1,49 @@
+import { User } from "@/types/user";
+import TattooWorkItem from "./partials/TattooWorkItem";
+import { useState } from "react";
+import { TattooWorkDetails } from "..";
+
+const TattooistGallery = ({ tattooist }: { tattooist: User }) => {
+  const [selectedTatooWork, setSelectedTattooWork] = useState<number | null>();
+  const tattooWork = tattooist.tattooWorks.find(
+    (e) => e.id === selectedTatooWork
+  );
+  return (
+    <>
+      <div className="my-4" key={tattooist.id}>
+        <div className="flex items-center gap-x-5 mb-4">
+          <img
+            src={tattooist.profilePicUrl}
+            className="rounded-full shadow-2xl bg-base-300 h-20 w-20"
+          />
+          <span className="text-2xl font-bold">{`${tattooist.firstName} ${tattooist.lastName}`}</span>
+        </div>
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
+          {tattooist.tattooWorks.map((tattooWork) => (
+            <TattooWorkItem
+              tattooWork={{ ...tattooWork, tattooist }}
+              key={tattooWork.id}
+              selected={selectedTatooWork === tattooWork.id}
+              onClick={() =>
+                setSelectedTattooWork((p) =>
+                  p === tattooWork.id ? null : tattooWork.id
+                )
+              }
+            />
+          ))}
+        </div>
+      </div>
+      {tattooWork && (
+        <TattooWorkDetails
+          tattooWork={{
+            ...tattooWork,
+            tattooist,
+          }}
+          onClose={() => setSelectedTattooWork(null)}
+        />
+      )}
+    </>
+  );
+};
+
+export default TattooistGallery;
