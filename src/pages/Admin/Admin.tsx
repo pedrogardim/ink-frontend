364997e1e3341f8dc82a7191d4359a8649726d63
Pedro Gardim <me@pedrogardim.com>
2023-11-17 16:13:14 +0100

    feat: add admin user table pagination

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,20 +1,24 @@
 import { useLazyGetUsersQuery } from "@/services";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import UserTable from "./partials/UserTable";
 
 const Admin = () => {
   const { entity } = useParams();
+  const [page, setPage] = useState(1);
   const [getUsers, { data: users, isLoading }] = useLazyGetUsersQuery();
 
   useEffect(() => {
-    getUsers({});
-  }, []);
+    getUsers({ page });
+  }, [page]);
 
   return (
     <div className="page overflow-hidden items-start">
       <h1 className="text-2xl font-bold capitalize mb-4">{entity}</h1>
-      {users && <UserTable users={users} />}
+      {isLoading && (
+        <span className="loading loading-dots loading-lg m-auto"></span>
+      )}
+      {users && <UserTable users={users} page={page} setPage={setPage} />}
     </div>
   );
 };
--- src/pages/Admin/partials/UserTable.tsx
+++ src/pages/Admin/partials/UserTable.tsx
@@ -1,14 +1,17 @@
 import dayjs from "dayjs";
 import Icon from "@mdi/react";
-import { mdiDelete, mdiPencil } from "@mdi/js";
+import { mdiArrowLeft, mdiArrowRight, mdiDelete, mdiPencil } from "@mdi/js";
 import { PaginationResponse } from "@/types/pagination";
 import type { User } from "@/types/user";
 
 interface UserTableProps {
   users: PaginationResponse<User>;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const userColums = [
+  "ID",
   "Name",
   "Email",
   "Role",
@@ -17,72 +20,92 @@ const userColums = [
   "Delete",
 ];
 
-const UserTable = ({ users }: UserTableProps) => {
-  if (!users) return;
+const UserTable = ({ users, setPage }: UserTableProps) => {
+  const handlePrevPage = () => {
+    setPage((p) => (p === 1 ? p : p - 1));
+  };
+  const handleNextPage = () => {
+    if (users.pageIndex === users.totalPages) return;
+    setPage((p) => p + 1);
+  };
   return (
-    <table className="table table-zebra">
-      {/* head */}
-      <thead>
-        <tr>
-          <th>
-            <label>
-              <input type="checkbox" className="checkbox" />
-            </label>
-          </th>
-          {userColums.map((e) => (
-            <th key={e}>{e}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {/* row 1 */}
-        {users.items.map((user) => (
-          <tr key={user.id}>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={user.profilePicUrl} alt={user.firstName} />
+    <div className="flex flex-col w-full">
+      <div className="flex-1 flex-shrink-1 overflow-scroll w-full">
+        <table className="table table-zebra mb-2 flex-1 w-full rounded-xl overflow-hidden">
+          {/* head */}
+          <thead>
+            <tr>
+              {userColums.map((e) => (
+                <th key={e}>{e}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {users.items.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={user.profilePicUrl} alt={user.firstName} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{`${user.firstName} ${user.lastName}`}</div>
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <div className="font-bold">{`${user.firstName} ${user.lastName}`}</div>
-                </div>
-              </div>
-            </td>
-            <td>{user.email}</td>
-            <td>{user.role}</td>
-            <td>
-              <div className="flex flex-col items-center">
-                <span>{dayjs(user.createdAt).format("DD/MM/YYYY HH:mm")}</span>
-                <span>{dayjs(user.updatedAt).format("DD/MM/YYYY HH:mm")}</span>
-              </div>
-            </td>
-            <td>
-              <button className="btn btn-circle btn-info">
-                <Icon path={mdiPencil} size={1} />
-              </button>
-            </td>
-            <td>
-              <button className="btn btn-circle btn-error">
-                <Icon path={mdiDelete} size={1} />
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-      {/* foot */}
-      <tfoot>
-        <div className="flex flex-row justify-center items-center">
-          <button></button>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  <div className="flex flex-col items-center">
+                    <span>
+                      {dayjs(user.createdAt).format("DD/MM/YYYY HH:mm")}
+                    </span>
+                    <span>
+                      {dayjs(user.updatedAt).format("DD/MM/YYYY HH:mm")}
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  <button className="btn btn-circle btn-info btn-sm">
+                    <Icon path={mdiPencil} size={0.8} />
+                  </button>
+                </td>
+                <td>
+                  <button className="btn btn-circle btn-error btn-sm">
+                    <Icon path={mdiDelete} size={0.8} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex flex-row justify-center items-center w-full gap-x-2">
+        <button
+          className="btn btn-sm btn-circle btn-ghost"
+          onClick={handlePrevPage}
+        >
+          <Icon path={mdiArrowLeft} size={1} />
+        </button>
+        <div className="flex flex-col items-center font-bold uppercase text-sm">
+          <span>{`Page ${users.pageIndex} - ${users.totalPages}`}</span>
+          <span>{`(${users.startIndex} - ${
+            users.startIndex + users.itemsPerPage - 1
+          } of ${users.totalItems})`}</span>
         </div>
-      </tfoot>
-    </table>
+
+        <button
+          className="btn btn-sm btn-circle btn-ghost"
+          onClick={handleNextPage}
+        >
+          <Icon path={mdiArrowRight} size={1} />
+        </button>
+      </div>
+    </div>
   );
 };
 
