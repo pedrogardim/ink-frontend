8312e35f6f0a0d18dc4a9ac687750ec55f0ef306
Pedro Gardim <me@pedrogardim.com>
2023-11-19 12:21:28 +0100

    feat: add sidemenu

--- src/common/layout/Header/Header.tsx
+++ src/common/layout/Header/Header.tsx
@@ -1,6 +1,7 @@
-import Icon from "@mdi/react";
+import { useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { mdiBell, mdiMenu, mdiMagnify } from "@mdi/js";
+import Icon from "@mdi/react";
+import { mdiBell, mdiMenu, mdiMagnify, mdiClose } from "@mdi/js";
 import { useSelector, useDispatch } from "@/store/hooks";
 import { logout } from "@/store/slices/userSlice";
 import { setSearchValue } from "@/store/slices/uiSlice";
@@ -9,62 +10,109 @@ const Header = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const { pathname } = useLocation();
+  const drawerController = useRef<HTMLInputElement>();
+
+  const closeDrawer = () =>
+    ((drawerController.current as HTMLInputElement).checked = false);
+
+  useEffect(() => {
+    closeDrawer();
+  }, [closeDrawer, pathname]);
+
+  const navItems = (
+    <>
+      {!pathname.includes("admin") && (
+        <>
+          {user && (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/appointments">My appointments</Link>
+              </li>
+            </>
+          )}
+          <li>
+            <Link to="/gallery">Artists</Link>
+          </li>
+        </>
+      )}
+      {pathname.includes("admin") && (
+        <>
+          <li>
+            <Link to="/admin/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/admin/appointments">Appointments</Link>
+          </li>
+          <li>
+            <Link to="/admin/tattooWorks">Tattoo works</Link>
+          </li>
+          <div className="relative ml-4">
+            <Icon
+              path={mdiMagnify}
+              size={0.8}
+              className="absolute top-2 left-2"
+            />
+            <input
+              type="text"
+              className="input input-bordered input-sm pl-7"
+              onChange={(e) => dispatch(setSearchValue(e.target.value))}
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <Icon path={mdiMenu} size={1} />
-        </button>
+      <div className="flex-none flex sm:hidden">
+        <div className="drawer z-10">
+          <input
+            id="my-drawer"
+            type="checkbox"
+            className="drawer-toggle"
+            ref={drawerController}
+          />
+          <div className="drawer-content">
+            <label
+              className="btn btn-square btn-ghost drawer-button"
+              htmlFor="my-drawer"
+            >
+              <Icon path={mdiMenu} size={1} />
+            </label>
+          </div>
+          <div className="drawer-side z-10">
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              <li>
+                <button
+                  className="btn btn-ghost normal-case text-2xl"
+                  onClick={closeDrawer}
+                >
+                  <Icon path={mdiClose} size={1} />
+                </button>
+              </li>
+              <li>
+                <Link className="text-2xl font-kenia" to="/">
+                  Ink
+                </Link>
+              </li>
+              {navItems}
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="">
         <Link className="btn btn-ghost normal-case text-2xl font-kenia" to="/">
           Ink
         </Link>
       </div>
-      <ul className="menu menu-horizontal px-1 mr-auto">
-        {!pathname.includes("admin") && (
-          <>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            {user && (
-              <li>
-                <Link to="/appointments">My appointments</Link>
-              </li>
-            )}
-            <li>
-              <Link to="/gallery">Artists</Link>
-            </li>
-          </>
-        )}
-        {pathname.includes("admin") && (
-          <>
-            <li>
-              <Link to="/admin/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/admin/appointments">Appointments</Link>
-            </li>
-            <li>
-              <Link to="/admin/tattooWorks">Tattoo works</Link>
-            </li>
-            <div className="relative ml-4">
-              <Icon
-                path={mdiMagnify}
-                size={0.8}
-                className="absolute top-2 left-2"
-              />
-              <input
-                type="text"
-                className="input input-bordered input-sm pl-7"
-                onChange={(e) => dispatch(setSearchValue(e.target.value))}
-              />
-            </div>
-          </>
-        )}
+      <ul className="menu menu-horizontal px-1 mr-auto hidden sm:flex">
+        {navItems}
       </ul>
       {user && (
-        <div className="flex-none">
+        <div className="flex-none ml-auto">
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="indicator">
@@ -121,7 +169,7 @@ const Header = () => {
         </div>
       )}
       {!user && (
-        <Link to="/login" className="btn btn-outline btn-primary">
+        <Link to="/login" className="btn btn-outline btn-primary ml-auto">
           Login
         </Link>
       )}
